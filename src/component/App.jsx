dfb656cbd70b448c6bc912e36ea538c7412db1dc
haritsf <haritsf@hotmail.com>
2019-12-21 06:33:00 +0700

    Update

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import Card from './Card';
+// import './../index.css';
+import '../assets/css/bootstrap.css';
+// import '../assets/js/custom.js';
 
 class App extends React.Component {
   render () {
     return (
-      <div className="App">
-      <Card
-        name="Harits Fathuddin"
-        role="FrontEnd"
-        nim="21120115130074" />
-      <Card
-        name="Aufal Marom"
-        role="BackEnd"
-        nim="21120115130069" />
-      <Card 
-        name="M. Adinugroho" 
-        role="DevOps"
-        nim="21120115140063" />
-      <Card 
-        name="Mukhlish A. Aziz" 
-        role="Network Engineer"
-        nim="21120115130052" />
-    </div>
+      <fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <Card
+                name="Harits Fathuddin"
+                role="FrontEnd"
+                nim="21120115130074" />
+              <Card
+                name="Aufal Marom"
+                role="BackEnd"
+                nim="21120115130069" />
+              <Card 
+                name="M. Adinugroho" 
+                role="DevOps"
+                nim="21120115140063" />
+              <Card 
+                name="Mukhlish A. Aziz" 
+                role="Network Engineer"
+                nim="21120115130052" />
+            </div>
+          </div>
+        </div>
+      </fragment>
     )
   }
 }
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import '.././index.css';
-import logo from '../images/logo_spectrum.png';
+import logo from '../assets/images/logo_spectrum.png';
 
 const Card = (props) => {
     return (
-        <div className="card">
-            <img className="logo" src={logo} alt="yah ga kepanggil"/>
-            <p className="title">{props.name}</p>
-            <p>{props.role}</p>
-            <p>{props.nim}</p>
+        <div className="card shadow rounded mx-3 my-3" style={{textAlign: 'center', display: 'inline-block', width: 250 + 'px'}}>
+            <div className="card-body">
+                <img src={logo} alt="yah ga kepanggil" style={{width: 50 + '%'}}/>
+                <h4 className="label-control">{props.name}</h4>
+                <p className="label-control">{props.role}</p>
+                <p className="label-control">{props.nim}</p>
+            </div>
         </div>
     )
 }
